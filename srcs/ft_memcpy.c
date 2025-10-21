3c1040b7b432e0ef57c43b1d6b1d386d5cd81e44
AquaKeynStud <arocca42student@gmail.com>
2025-10-21 18:21:48 +0200

    Optimized memcpy for speed (for cub3d...)

--- srcs/ft_memcpy.c
+++ srcs/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: arocca <arocca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:37:15 by arocca            #+#    #+#             */
-/*   Updated: 2025/09/08 10:32:51 by arocca           ###   ########.fr       */
+/*   Updated: 2025/10/21 18:20:23 by arocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,23 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	const char	*nsrc;
-	char		*ndest;
+	size_t				i;
+	unsigned char		*cdst;
+	unsigned long		*ldst;
+	const unsigned char	*csrc;
+	const unsigned long	*lsrc;
 
 	if (!dest && !src)
 		return (NULL);
-	ndest = (char *)dest;
-	nsrc = (const char *)src;
-	while (n)
-	{
-		ndest[n - 1] = nsrc[n - 1];
-		n--;
-	}
+	ldst = (unsigned long *)dest;
+	lsrc = (const unsigned long *)src;
+	i = n / sizeof(unsigned long);
+	while (i--)
+		*ldst++ = *lsrc++;
+	cdst = (unsigned char *)ldst;
+	csrc = (const unsigned char *)lsrc;
+	i = n % sizeof(unsigned long);
+	while (i--)
+		*cdst++ = *csrc++;
 	return (dest);
 }
